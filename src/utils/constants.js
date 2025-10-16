2f3b9a75562f119a908a27d31e1721a25c8be215
SIn <122032993+MACAT300@users.noreply.github.com>
2025-10-16 11:47:02 +0800

    1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@
 // export const API_URL = "http://localhost:5123/";
 // mine
 export const API_URL = import.meta.env.DEV
-  ? "http://localhost:5555/"
+  ? "http://localhost:5555/api/"
   : "https://b15-rentai.mak3r.dev/api/";
